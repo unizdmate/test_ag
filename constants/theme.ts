73c1fc2e5ccdbf74d1828a9d1b2e773694ffd5ef
Mate Krezic <mate.krezic@deepit.hr>
2024-04-04 18:03:27 +0200

    Add theme file

---
+++ constants/theme.ts
@@ -0,0 +1,37 @@
+const colors = {
+  primary: '#673AB7',
+  secondary: '#FFD700',
+  accent: '#000000',
+  background: '#FFFFFF',
+  danger: '#FF0000',
+};
+
+const fonts = {
+  h1: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: colors.primary,
+  },
+  h2: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.primary,
+  },
+  body1: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    color: colors.primary,
+  },
+  body2: {
+    fontSize: 14,
+    fontWeight: 'normal',
+    color: colors.primary,
+  },
+};
+
+const sizings = {
+  baseMargin: 4,
+  basePadding: 8,
+};
+
+export {colors, fonts, sizings};
