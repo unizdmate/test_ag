0157e9782f8a9a1b82c1606e06ccb5ecf7bcb797
Mate Krezic <mate.krezic@deepit.hr>
2024-04-09 20:07:55 +0200

    Version bump

--- screens/UsersListScreen/components/EmptyList.tsx
+++ screens/UsersListScreen/components/EmptyList.tsx
@@ -36,6 +36,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     gap: sizings.baseGap * 8,
+    paddingVertical: sizings.basePadding * 12,
   },
   title: {
     fontSize: 16,
